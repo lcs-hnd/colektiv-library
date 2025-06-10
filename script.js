428f8463f8f85fa2a918704aa396dad28c82882a
Lucas Honda <103671864+lcs-hnd@users.noreply.github.com>
2025-06-10 07:23:20 -0400

    feat: implement granular transitions and streamline styles
    overhauled ui for viewport responsiveness, enhanced visuals, and improved maintainability.
    
    html:
    - introduced new container for future page fade-in.
    - standardized css class names to bem-like convention.
    
    css:
    - refactored styles: combined properties, removed unused/redundant css.
    - refined element animations for smoother transitions.
    - added media query for responsive adjustments.
    - implemented `active-screen` class for js dynamic visibility.
    - replaced body fade-in with granular element fade-ins.
    
    js:
    - integrated `DOMContentLoaded` and `transitionend` listeners for screen transitions.
    - utilized `active-screen` class for active content management.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-window.onload = function() {
-    document.body.classList.add('fade-in');
-};
-
 const myLibrary = [];
 
 function Book() {
@@ -12,4 +8,35 @@ function addBookToLibrary() {
 
 };
 
+// loads first screen and transition to the library content
+document.addEventListener('DOMContentLoaded', () => {
+    const introScreen = document.querySelector('.intro');
+    const introButton = document.querySelector('.intro-button');
+    const libraryScreen = document.querySelector('.library-content');
 
+    // assign class to first screen
+    introScreen.classList.add('active-screen');
+
+    // begins transition to the main content of the library
+    introButton.addEventListener('click', () => {
+
+        // checking trasitionends in order 
+        const introButtonTransform = function(event) {
+            if (event.propertyName === 'transform') {
+                introButton.removeEventListener('transitionend', introButtonTransform);
+                introScreen.classList.remove('active-screen');
+
+                const introScreenFadeEnd = function(event) {
+                    if(event.propertyName === 'opacity'){
+                        introScreen.removeEventListener('transitionend', introScreenFadeEnd);
+
+                        libraryScreen.classList.add('active-screen');
+                        introScreen.remove();
+                    }
+                };
+                introScreen.addEventListener('transitionend', introScreenFadeEnd);
+            }
+        };
+        introButton.addEventListener('transitionend', introButtonTransform);
+    });
+});
